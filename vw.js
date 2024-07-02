6a41b6a78788f8df70cda12df0560270094dc5b0
oba-mori <99237421+oba-mori@users.noreply.github.com>
2024-07-02 13:53:09 +0900

    Delete vw.js

--- vw.js
+++
@@ -1,6 +0,0 @@
-const setVw = function() {
-  const vw = document.documentElement.clientWidth / 100;
-  document.documentElement.style.setProperty('--vw', `${vw}px`);
-}
-window.addEventListener('DOMContentLoaded', setVw);
-window.addEventListener('resize', setVw);
