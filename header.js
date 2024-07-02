aadf5db58cb3878b4a33569ddf4e10363e54148c
oba-mori <99237421+oba-mori@users.noreply.github.com>
2024-07-02 13:52:48 +0900

    Delete header.js

--- header.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $('header').load('header.html header', function(response, status, xhr) {
-      if (status == "error") {
-          console.log("Error: " + xhr.status + ": " + xhr.statusText);
-      }
-  });
-});
